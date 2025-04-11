435b24dbd1dee8d3671096c8f303b2a60aeeb39f
Carson Gee <carson.gee@datarobot.com>
2025-04-10 21:17:13 -0600

    Corrected formatting in tests

--- template/{{fastapi_app_name}}/tests/test_main.py
+++ template/{{fastapi_app_name}}/tests/test_main.py
@@ -17,15 +17,18 @@ from app.main import app
 
 client = TestClient(app)
 
+
 def test_index():
     response = client.get("/")
     assert response.status_code == 200
     assert response.headers["content-type"] == "text/html; charset=utf-8"
 
+
 def test_welcome():
     response = client.get("/api/v1/welcome")
     assert response.status_code == 200
     assert response.json() == {"message": "Welcome Engineer!"}
+
 
 def test_health():
     response = client.get("/health")
