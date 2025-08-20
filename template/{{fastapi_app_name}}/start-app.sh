8fd4929fe1a445acf4a65a06b6bf7ac54404965c
Carson Gee <carson.gee@datarobot.com>
2025-08-19 18:39:36 -0600

    Adjusted uvicorn to listen on all adapters

--- template/{{fastapi_app_name}}/start-app.sh
+++ template/{{fastapi_app_name}}/start-app.sh
@@ -20,4 +20,4 @@ if [[ $workers -lt 2 ]]; then
 fi
 
 echo "Starting App with ${workers} workers"
-uv run uvicorn app.main:app --workers "$workers" --port 8080 --proxy-headers --timeout-keep-alive 300
+uv run uvicorn app.main:app --workers "$workers" --host 0.0.0.0 --port 8080 --proxy-headers --timeout-keep-alive 300
