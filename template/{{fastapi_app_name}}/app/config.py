444afb4f6716b46ea9b080045399997095a11239
Carson Gee <carson.gee@datarobot.com>
2025-12-29 13:57:37 -0700

    Corrected import paths

--- template/{{fastapi_app_name}}/app/config.py
+++ template/{{fastapi_app_name}}/app/config.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from datarobot.core.config import DataRobotAppFrameworkBaseSettings
-from telemetry import FormatType, LogLevel
+from app.telemetry import FormatType, LogLevel
 
 
 class Config(DataRobotAppFrameworkBaseSettings):
