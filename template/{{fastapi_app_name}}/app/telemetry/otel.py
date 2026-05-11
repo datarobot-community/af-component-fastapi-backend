aa1cfb3716c28dc179e35dd348373c7d4f9da5ed
elvinagam <eaghammad@gmail.com>
2026-05-11 11:31:49 -0400

    Fix unused type ignore

--- template/{{fastapi_app_name}}/app/telemetry/otel.py
+++ template/{{fastapi_app_name}}/app/telemetry/otel.py
@@ -75,7 +75,7 @@ except ImportError:
 try:
     from opentelemetry.instrumentation.httpx import HTTPXClientInstrumentor
 except ImportError:
-    HTTPXClientInstrumentor = None  # type: ignore[assignment, misc]
+    HTTPXClientInstrumentor = None  # type: ignore[assignment]
 
 P = ParamSpec("P")
 T = TypeVar("T")
