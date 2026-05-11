024e43b75f305580e99c83b8082890490409d943
elvinagam <eaghammad@gmail.com>
2026-05-11 11:27:21 -0400

    Fix import ordering

--- template/{{fastapi_app_name}}/app/config.py
+++ template/{{fastapi_app_name}}/app/config.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from datarobot.core.config import DataRobotAppFrameworkBaseSettings
+
 from app.telemetry import FormatType, LogLevel
 
 
